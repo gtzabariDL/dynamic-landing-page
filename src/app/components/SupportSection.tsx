f703082e49f4cd2cf1fecd7b67f96aa8ffbd593a
gtzabariDL <gtzabari@doorloop.com>
2025-05-21 07:49:42 +0300

    Fix most issues in the support section

--- src/app/components/SupportSection.tsx
+++ src/app/components/SupportSection.tsx
@@ -2,9 +2,18 @@ import Image from 'next/image';
 
 export default function SupportSection() {
   return (
-    <section className="flex flex-col md:flex-row items-start md:items-center justify-between bg-white px-6 md:px-16 py-14 space-y-5 md:space-y-0 w-full">
-      {/* Left side - Content */}
-      <div className="w-full md:w-1/2 flex flex-col space-y-5">
+    <section className="flex flex-col-reverse md:flex-row items-start md:items-center md:space-x-24 bg-white px-6 md:px-16 py-14 md:space-y-0 w-full">
+      <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+        <Image
+          src="supportStats/supportStats.svg"
+          alt="Support Statistics"
+          width={520}
+          height={660}
+          className="w-auto h-auto"
+        />
+      </div>
+
+      <div className="w-full md:w-1/2 flex flex-col space-y-5 mb-12 md:mb-0 max-w-[500px]">
         {/* Satisfaction Rating */}
         <div className="text-[#A8A8A8] text-sm font-medium">
           98.6% CUSTOMER SATISFACTION RATING
@@ -23,7 +32,7 @@ export default function SupportSection() {
         </p>
 
         {/* Support Features List */}
-        <ul className="text-gray-800 space-y-4">
+        <ul className="text-gray-800 grid grid-cols-1 md:grid-cols-2 gap-4">
           {[
             'Support included',
             'Help Center',
@@ -38,21 +47,11 @@ export default function SupportSection() {
         </ul>
 
         {/* CTA button */}
-        <button className="mt-4 bg-[#01CC74] text-white py-2 px-6 rounded-md font-medium shadow hover:bg-[#00b27f] transition-colors w-fit">
+        <button className="mt-4 text-sm bg-[#01CC74] text-white rounded-md font-medium shadow hover:bg-[#00b27f] transition-colors w-36 h-14">
           Request a Demo
         </button>
       </div>
 
-      {/* Right side - Image */}
-      <div className="w-full md:w-1/2 flex justify-center md:justify-end">
-        <Image 
-          src="supportStats.svg" 
-          alt="Support Statistics" 
-          width={350} 
-          height={300}
-          className="w-auto h-auto" 
-        />
-      </div>
     </section>
   );
 } 
