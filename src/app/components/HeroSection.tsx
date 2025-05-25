dc5a88eab34491add301ba9aefd8fe08140b8940
gtzabariDL <gtzabari@doorloop.com>
2025-05-25 12:09:03 +0300

    Add background image to the hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -15,69 +15,71 @@ export default function HeroSection() {
     window.open(`https://demo.doorloop.com/demo/additional-info?email=${email}`);
   };
 
-
   return (
-    <section className="flex-grow flex-col flex items-center justify-center bg-[#2F3E83] px-4 py-8 w-full h-[98vh]">
-      <div className="flex items-center justify-center mb-6">
-        <DoorLoopLogo color="white" width={200} height={54} />
-      </div>
-
-      <h1 className="text-white text-4xl md:text-5xl text-center font-normal mb-6">
-        The <span className="font-bold">Highest-Rated</span> Property Management Software
-      </h1>
-
-      <span className="text-center text-white max-w-3xl text-base md:text-lg">
-        Save 15 hours a month, put your rental portfolio on autopilot and make accounting a breeze.
-      </span>
-
-      <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-2xl space-y-2 md:space-y-0 md:my-12 ">
-        <div className="flex items-center w-full md:w-80 bg-white rounded-md md:rounded-md md:rounded-r-none px-3 py-2 shadow-sm h-14">
-          <Image
-            src="email-icon.svg"
-            alt="Email"
-            width={24}
-            height={24}
-            className="text-gray-400 mr-3 ml-1"
-          />
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            placeholder="Your Email"
-            className="w-full outline-none text-gray-400 text-base placeholder:text-gray-400"
-          />
+    <section className="relative flex-grow flex-col flex items-center justify-center px-4 py-8 w-full h-[98vh]">
+      <div className="fixed top-0 left-0 w-full h-[98vh] bg-[url('/hero-background.svg')] bg-no-repeat bg-cover -z-10 hidden md:block" />
+      <div className="relative z-10 w-full h-full flex flex-col items-center justify-center">
+        <div className="flex items-center justify-center mb-6">
+          <DoorLoopLogo color="white" width={200} height={54} />
         </div>
-        <button onClick={onRequestDemo} className="w-full md:w-52 md:px-8 h-14 bg-[#01cc74] text-white py-2 rounded-md md:rounded-l-none md:rounded-r-md font-medium shadow hover:bg-[#00b27f] transition-colors whitespace-nowrap">
-          Request a Demo
-        </button>
-      </div>
 
-      <div className="flex flex-col md:flex-row justify-center items-center w-full max-w-sm md:max-w-full space-y-2 mt-6 space-x-5">
-        <Image
-          className="dark:invert"
-          src="saleIcon.svg"
-          alt="Spring Sale Icon"
-          width={120}
-          height={60}
-          priority
-        />
-        <span className="text-white text-center text-sm font-normal mt-2">
-          A fresh path to real growth. Get one month free + zero onboarding fees on all annual plans. Hurry—offer ends <span className="font-bold">Friday May 23rd</span>.
+        <h1 className="text-white text-4xl md:text-5xl text-center font-normal mb-6">
+          The <span className="font-bold">Highest-Rated</span> Property Management Software
+        </h1>
+
+        <span className="text-center text-white max-w-3xl text-base md:text-lg">
+          Save 15 hours a month, put your rental portfolio on autopilot and make accounting a breeze.
         </span>
-      </div>
 
-      {/* Review Platforms Grid */}
-      <div className="grid grid-cols-2 md:flex md:items-center max-w-6xl mt-8 gap-6 md:gap-10">
-        {reviewPlatforms.map((platform) => (
+        <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-2xl space-y-2 md:space-y-0 md:my-12 ">
+          <div className="flex items-center w-full md:w-80 bg-white rounded-md md:rounded-md md:rounded-r-none px-3 py-2 shadow-sm h-14">
+            <Image
+              src="email-icon.svg"
+              alt="Email"
+              width={24}
+              height={24}
+              className="text-gray-400 mr-3 ml-1"
+            />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Your Email"
+              className="w-full outline-none text-gray-400 text-base placeholder:text-gray-400"
+            />
+          </div>
+          <button onClick={onRequestDemo} className="w-full md:w-52 md:px-8 h-14 bg-[#01cc74] text-white py-2 rounded-md md:rounded-l-none md:rounded-r-md font-medium shadow hover:bg-[#00b27f] transition-colors whitespace-nowrap">
+            Request a Demo
+          </button>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center items-center w-full max-w-sm md:max-w-full space-y-2 mt-6 space-x-5">
           <Image
-            key={platform.alt}
-            src={platform.src}
-            alt={platform.alt}
+            className="dark:invert"
+            src="saleIcon.svg"
+            alt="Spring Sale Icon"
             width={120}
-            height={40}
-            className="w-auto h-auto"
+            height={60}
+            priority
           />
-        ))}
+          <span className="text-white text-center text-sm font-normal mt-2">
+            A fresh path to real growth. Get one month free + zero onboarding fees on all annual plans. Hurry—offer ends <span className="font-bold">Friday May 23rd</span>.
+          </span>
+        </div>
+
+        {/* Review Platforms Grid */}
+        <div className="grid grid-cols-2 md:flex md:items-center max-w-6xl mt-8 gap-6 md:gap-10">
+          {reviewPlatforms.map((platform) => (
+            <Image
+              key={platform.alt}
+              src={platform.src}
+              alt={platform.alt}
+              width={120}
+              height={40}
+              className="w-auto h-auto"
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
