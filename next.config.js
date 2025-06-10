9d98a05f5c44de401d7717700e49f3c91a7e1e55
Amir Ben Shimol <ashimol@doorloop.com>
2025-06-10 17:43:49 +0300

    Update next.config.js to disable trailing slashes for improved URL handling

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'dynamic-landing-page';
+
 const nextConfig = {
 	reactStrictMode: false,
 	typescript: {
@@ -19,14 +22,17 @@ const nextConfig = {
 		],
 	},
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+	
 	output: 'export',
-	trailingSlash: false,
+	trailingSlash: true,
 	// Performance optimizations
 	poweredByHeader: false,
 	compress: true,
 	experimental: {
 		optimizePackageImports: ['react-helmet-async', 'react-i18next'],
 	},
+	basePath: isProd ? `/${repoName}` : '',
+	assetPrefix: isProd ? `/${repoName}/` : '',
 	// basePath: process.env.NODE_ENV === 'production' ? '/dynamic-landing-page' : '',
 	// assetPrefix: process.env.NODE_ENV === 'production' ? '/dynamic-landing-page' : '',
 };
